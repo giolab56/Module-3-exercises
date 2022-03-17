8f0f96b82ef7473b4c5bf9adf6da616b28abca37
giolab56 <100443438+giolab56@users.noreply.github.com>
2022-03-17 17:10:24 -0400

    Add files via upload

---
+++ FortuneCookie.py
@@ -0,0 +1,16 @@
+#Fortune
+import random
+
+fortune = random.randint(1,5)
+
+if fortune == 1:
+    print("Yes... for now...")
+elif fortune == 2:
+    print("Your day will be good!")
+elif fortune == 3:
+    print("I am sensing a disturbance")
+elif fortune == 4:
+    print("Oh yeah the fortune. Lucky I guess.")
+elif fortune == 5:
+    print("You will win the lotto!")
+
